04038a38531085d51a1e0085d542cf52b295ba29
DevtheJay <99936446+DevtheJay@users.noreply.github.com>
2024-03-14 23:44:53 -0700

    Updating Tests
    Worked on with Jerico and Dimir

--- lexer/TestProject1/TokenizerTests.cs
+++ lexer/TestProject1/TokenizerTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 [TestClass]
 public class TokenizerTests
 {
-    //KEY WORDS
+    //Keywords Tests
     [TestMethod]
     public void Tokenizer_ShouldIdentifyVarToken()
     {
@@ -50,7 +50,318 @@ public class TokenizerTests
         Assert.AreEqual(TokenType.VoidToken, token.Type);
         Assert.AreEqual("", token.Lexeme);
     }
+
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesThisKeyword()
+    {
+        var tokenizer = new Tokenizer("this");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.ThisToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesTrueKeyword()
+    {
+        var tokenizer = new Tokenizer("true");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.TrueToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesFalseKeyword()
+    {
+        var tokenizer = new Tokenizer("false");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.FalseToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesPrintLnKeyword()
+    {
+        var tokenizer = new Tokenizer("println");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.PrintlnToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesNewKeyword()
+    {
+        var tokenizer = new Tokenizer("new");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.NewToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesWhileKeyword()
+    {
+        var tokenizer = new Tokenizer("while");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.WhileToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesBreakKeyword()
+    {
+        var tokenizer = new Tokenizer("break");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.BreakToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesReturnKeyword()
+    {
+        var tokenizer = new Tokenizer("return");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.ReturnToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesIfKeyword()
+    {
+        var tokenizer = new Tokenizer("if");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.IfToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesElseKeyword()
+    {
+        var tokenizer = new Tokenizer("else");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.ElseToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesMethodKeyword()
+    {
+        var tokenizer = new Tokenizer("method");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.MethodToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesInitKeyword()
+    {
+        var tokenizer = new Tokenizer("init");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.InitToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesSuperKeyword()
+    {
+        var tokenizer = new Tokenizer("super");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.SuperToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    /*
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
+    //Operator Tests
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesPlusOperator()
+    {
+        var tokenizer = new Tokenizer("+");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.AddToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesEqualsOperator()
+    {
+        var tokenizer = new Tokenizer("=");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.EqualsToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesDoubleEqualsOperator()
+    {
+        var tokenizer = new Tokenizer("==");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.DoubleEqualsToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesSubOperator()
+    {
+        var tokenizer = new Tokenizer("-");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.SubToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
     
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesMultOperator()
+    {
+        var tokenizer = new Tokenizer("*");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.MultToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesDivOperator()
+    {
+        var tokenizer = new Tokenizer("/");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.DivToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesAndOperator()
+    {
+        var tokenizer = new Tokenizer("&&");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.AndToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesOrOperator()
+    {
+        var tokenizer = new Tokenizer("||");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.OrToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesSemicolonOperator()
+    {
+        var tokenizer = new Tokenizer(";");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.SemicolonToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesOpenParenthesisOperator()
+    {
+        var tokenizer = new Tokenizer("(");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.OpenParenthesisToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesNotEqualsOperator()
+    {
+        var tokenizer = new Tokenizer("!=");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.NotEqualsToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesOpenBracketOperator()
+    {
+        var tokenizer = new Tokenizer("{");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.OpenBracketToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+
+
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesClosedBracketOperator()
+    {
+        var tokenizer = new Tokenizer("}");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.ClosedBracketToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+
+
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesCloseParenthesisOperator()
+    {
+        var tokenizer = new Tokenizer(")");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.CloseParenthesisToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+
+
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesCommaTokenOperator()
+    {
+        var tokenizer = new Tokenizer(",");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.CommaToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    /*
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
 
     [TestMethod]
     public void Tokenizer_CorrectlyIdentifiesIntegerLiteral()
@@ -62,16 +373,7 @@ public class TokenizerTests
         Assert.AreEqual(TokenType.IntegerLiteral, token.Type);
         Assert.AreEqual("12345", token.Lexeme);
     }
-    [TestMethod]
-    public void Tokenizer_CorrectlyIdentifiesPlusOperator()
-    {
-        var tokenizer = new Tokenizer("+");
-        var token = tokenizer.Get();
-
-        Assert.IsNotNull(token);
-        Assert.AreEqual(TokenType.AddToken, token.Type);
-        Assert.AreEqual("", token.Lexeme);
-    }
+   
     [TestMethod]
     public void Tokenizer_IgnoresWhitespace()
     {
--- lexer/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ lexer/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e350e593fc09d36ca600bd592adf642e09a282")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70884e342a2a118224735f6e13ea18441dc4d958")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
