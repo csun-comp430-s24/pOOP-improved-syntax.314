70884e342a2a118224735f6e13ea18441dc4d958
DevtheJay <99936446+DevtheJay@users.noreply.github.com>
2024-03-14 23:14:48 -0700

    Updating Tests

--- lexer/TestProject1/TokenizerTests.cs
+++ lexer/TestProject1/TokenizerTests.cs
@@ -62,5 +62,64 @@ public class TokenizerTests
         Assert.AreEqual(TokenType.IntegerLiteral, token.Type);
         Assert.AreEqual("12345", token.Lexeme);
     }
+    [TestMethod]
+    public void Tokenizer_CorrectlyIdentifiesPlusOperator()
+    {
+        var tokenizer = new Tokenizer("+");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.AddToken, token.Type);
+        Assert.AreEqual("", token.Lexeme);
+    }
+    [TestMethod]
+    public void Tokenizer_IgnoresWhitespace()
+    {
+        var tokenizer = new Tokenizer(" ");
+        var token = tokenizer.Get();
+
+        Assert.IsNotNull(token);
+        Assert.AreEqual(TokenType.Space, token.Type);
+    }
+    [TestMethod]
+    public void Tokenizer_RejectsInvalidInput()
+    {
+        var tokenizer = new Tokenizer("@");
+        var token = tokenizer.Get();
+
+        Assert.IsNull(token);
+    }
+    
+    [TestMethod]
+    public void Tokenizer_HandlesEndOfFile()
+    {
+        var tokenizer = new Tokenizer("var x");
+        Token token;
+        while ((token = tokenizer.Get()) != null)
+        {
+            // process tokens
+        }
+        Assert.IsNull(token);
+    }
+
+    [TestMethod]
+    public void Tokenizer_HandlesMultiLineInput()
+    {
+        var tokenizer = new Tokenizer("var x\nint y");
+        var token1 = tokenizer.Get();
+        var token2 = tokenizer.Get();
+        // Assertions for both tokens
+    }
+    [TestMethod]
+    public void Tokenizer_PeekDoesNotAdvance()
+    {
+        var tokenizer = new Tokenizer("var x");
+        var peekedToken = tokenizer.Peek();
+        var token = tokenizer.Get();
+
+        Assert.AreEqual(peekedToken.Type, token.Type);
+        Assert.AreEqual(peekedToken.Lexeme, token.Lexeme);
+    }
+
 
 }
--- lexer/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ lexer/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f74f26e5d4615a30d9f1ca8b1c154a61c4b8c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e350e593fc09d36ca600bd592adf642e09a282")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
