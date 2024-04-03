9ac8be42f73f2ad3d7aec9c091a48c26baa7ca9a
hii314 <35716033+hii314@users.noreply.github.com>
2024-04-02 21:58:41 -0700

    idk what these are

--- lexer/430 project/obj/Debug/net6.0/430 project.AssemblyInfo.cs
+++ lexer/430 project/obj/Debug/net6.0/430 project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("430 project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5643c6d747d95033fd744841e21201b4d11da40")]
 [assembly: System.Reflection.AssemblyProductAttribute("430 project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("430 project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
