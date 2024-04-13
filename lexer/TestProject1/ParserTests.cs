f410ab9dd7b2673cc6c165e6e1d8710d56a65ef4
Jericho Raz <jericho.raz.584@my.csun.edu>
2024-04-12 22:27:59 -0700

    Added more Test Cases for Parser - Test for VarDec Statments

--- lexer/TestProject1/ParserTests.cs
+++ lexer/TestProject1/ParserTests.cs
@@ -21,4 +21,23 @@ public class ParserTests
         Assert.IsTrue(stmt.left.Equals(new Parser.Identifier("x")));
         Assert.IsTrue(stmt.right is Parser.BinopExp);
     }
+
+    [TestMethod]
+    public void Parser_ShouldParseVarDecStmt()
+    {
+        string source = "bool x;";
+        Tokenizer tokenizer = new Tokenizer(source);
+        List<Token> tokens = tokenizer.GetAllTokens();
+
+        Parser parser = new Parser(tokens);
+        var ast = parser.ParseProgram(0);
+        Parser.Code code = (Parser.Code)ast.parseResult;
+
+        Parser.VarDecStmt stmt = (Parser.VarDecStmt)code.stmts[0];
+
+        Assert.IsNotNull(stmt);
+        Assert.IsTrue(stmt.varType is Parser.BooleanType);
+        Assert.IsTrue(stmt.varIdentifier is Parser.Identifier);
+        Assert.IsTrue(stmt.varIdentifier.value.Equals("x"));
+    }
 }
