c51962c269d45b55cdb5f20c74b06a497b9af630
hii314 <35716033+hii314@users.noreply.github.com>
2024-04-12 22:47:13 -0700

    Added test cases for block stmt, break stmt, void type, bool type, int type

--- lexer/TestProject1/ParserTests.cs
+++ lexer/TestProject1/ParserTests.cs
@@ -40,4 +40,78 @@ public class ParserTests
         Assert.IsTrue(stmt.varIdentifier is Parser.Identifier);
         Assert.IsTrue(stmt.varIdentifier.value.Equals("x"));
     }
+
+    [TestMethod]
+    public void Parser_ShouldParseBreakStmt()
+    {
+        string source = "break;";
+        Tokenizer tokenizer = new Tokenizer(source);
+        List<Token> tokens = tokenizer.GetAllTokens();
+
+        Parser parser = new Parser(tokens);
+        var ast = parser.ParseProgram(0);
+        Parser.Code code = (Parser.Code)ast.parseResult;
+
+        Parser.VarDecStmt stmt = (Parser.VarDecStmt)code.stmts[0];
+
+        Assert.IsNotNull(stmt);
+    }
+
+    [TestMethod]
+    public void Parser_ShouldParseBlockStmt()
+    {
+        string source = "{bool x;}";
+        Tokenizer tokenizer = new Tokenizer(source);
+        List<Token> tokens = tokenizer.GetAllTokens();
+
+        Parser parser = new Parser(tokens);
+        var ast = parser.ParseProgram(0);
+        Parser.Code code = (Parser.Code)ast.parseResult;
+
+        Parser.VarDecStmt stmt = (Parser.VarDecStmt)code.stmts[0];
+
+        Assert.IsNotNull(stmt);
+        Assert.IsTrue(stmt.varType is Parser.BooleanType);
+        Assert.IsTrue(stmt.varIdentifier is Parser.Identifier);
+        Assert.IsTrue(stmt.varIdentifier.value.Equals("x"));
+    }
+
+    [TestMethod]
+    public void Parser_ShouldParseint()
+    {
+        string source = "int x;";
+        Tokenizer tokenizer = new Tokenizer(source);
+        List<Token> tokens = tokenizer.GetAllTokens();
+
+        Parser parser = new Parser(tokens);
+        var ast = parser.ParseProgram(0);
+        Parser.Code code = (Parser.Code)ast.parseResult;
+
+        Parser.VarDecStmt stmt = (Parser.VarDecStmt)code.stmts[0];
+
+        Assert.IsNotNull(stmt);
+        Assert.IsTrue(stmt.varType is Parser.IntType);
+        Assert.IsTrue(stmt.varIdentifier is Parser.Identifier);
+        Assert.IsTrue(stmt.varIdentifier.value.Equals("x"));
+    }
+
+    [TestMethod]
+    public void Parser_ShouldParseVoidStmt()
+    {
+        string source = "void x;";
+        Tokenizer tokenizer = new Tokenizer(source);
+        List<Token> tokens = tokenizer.GetAllTokens();
+
+        Parser parser = new Parser(tokens);
+        var ast = parser.ParseProgram(0);
+        Parser.Code code = (Parser.Code)ast.parseResult;
+
+        Parser.VarDecStmt stmt = (Parser.VarDecStmt)code.stmts[0];
+
+        Assert.IsNotNull(stmt);
+        Assert.IsTrue(stmt.varType is Parser.VoidType);
+        Assert.IsTrue(stmt.varIdentifier is Parser.Identifier);
+        Assert.IsTrue(stmt.varIdentifier.value.Equals("x"));
+    }
+
 }
