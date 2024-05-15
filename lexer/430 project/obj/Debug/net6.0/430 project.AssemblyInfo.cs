13f5b8d1c596fe2fc18628ae4e02019f14d3d619
hii314 <35716033+hii314@users.noreply.github.com>
2024-05-14 19:05:31 -0700

    Attempted to add typechecker file based off of Deweys code that was translated in C#

--- lexer/430 project/obj/Debug/net6.0/430 project.AssemblyInfo.cs
+++ lexer/430 project/obj/Debug/net6.0/430 project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("430 project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b861110705290207400f5fe720e0452b7694a5eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+741a36bc93c3eff415e800c4bd300b8fbea980df")]
 [assembly: System.Reflection.AssemblyProductAttribute("430 project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("430 project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
