6e38e07f7b2b09ae5ca96b39211664b051e4d57f
hii314 <35716033+hii314@users.noreply.github.com>
2024-05-15 20:38:56 -0700

    idk what these are

--- lexer/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ lexer/TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df3e2e7cb882d0bd2e96bcf18546fbea2e322b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13f5b8d1c596fe2fc18628ae4e02019f14d3d619")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
